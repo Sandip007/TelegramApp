6065e6369c6cdea863b3ffa39160745e186cabdc
Sandip Das <sandipdas@rydigicom.com>
2024-11-02 20:22:10 +0530

    Add Stream Chat SDK

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,7 +3,12 @@
 // Define Global Provider
 
 import {Slot} from "expo-router";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
-  return <Slot />;
+  return(
+      <GestureHandlerRootView>
+        <Slot />
+      </GestureHandlerRootView>
+  );
 }
